581d48e416657303ffad4c8680e42577d798a51d
Francisco Márquez <francisco.marquez@nahitekdigital.com>
2020-11-30 22:35:52 +0100

    Initial version

---
+++ src/com/company/Book.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Book {
+    public String getTitle() {
+        return "A great book";
+    }
+
+    public String getAuthor() {
+        return "John Doe";
+    }
+
+    public void printCurrentPage() {
+        System.out.println("current page content");
+    }
+}
---
+++ src/com/company/Client.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Client {
+    public Client() {
+        final Book book = new Book();
+        book.printCurrentPage();
+    }
+}
